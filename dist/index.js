5ac4eb52400b3c416e90827b21a203d029d07f61
MollyDesign <b1207782646@163.com>
2022-12-19 17:53:58 +0800

    red

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,9 @@ import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
 const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(1,1,1)
+const geometry = new THREE.BoxGeometry(10,1,1)
 const material = new THREE.MeshBasicMaterial({
-    color:0x00ff00
+    color:0xff0000
 })
 const mesh = new THREE.Mesh(geometry,material)
 scene.add(mesh)
