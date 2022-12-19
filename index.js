fde19890a905dedd918782ca83e2335797383a02
MollyDesign <b1207782646@163.com>
2022-12-19 17:48:43 +0800

    Updates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
 const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(1,1,1)
+const geometry = new THREE.BoxGeometry(10,1,1)
 const material = new THREE.MeshBasicMaterial({
     color:0x00ff00
 })
