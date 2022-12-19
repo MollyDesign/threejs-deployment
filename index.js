b1fcf073dd1e9f44f83a5feffbc5a9a3ea95fecf
MollyDesign <b1207782646@163.com>
2022-12-19 17:50:50 +0800

    Updates

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const scene = new THREE.Scene()
 
 const geometry = new THREE.BoxGeometry(10,1,1)
 const material = new THREE.MeshBasicMaterial({
-    color:0x00ff00
+    color:0xff0000
 })
 const mesh = new THREE.Mesh(geometry,material)
 scene.add(mesh)
